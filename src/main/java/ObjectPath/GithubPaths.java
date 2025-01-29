0e18247c66e665c81e08217e0bd9a197ff642fc4
shatha_alharbi <shatha3456alharbi@gmail.com>
2025-01-29 21:09:08 +0300

    added new features

--- src/main/java/ObjectPath/GithubPaths.java
+++ src/main/java/ObjectPath/GithubPaths.java
@@ -5,11 +5,6 @@ import org.openqa.selenium.By;
 public class GithubPaths {
     public static final By signIn = By.xpath("/html/body/div[1]/div[3]/header/div/div[2]/div/div/div/a");
 
-    public static final By username = By.xpath("//input[@id='login_field']");
-
-    public static final By password = By.xpath("//input[@id='password']");
-
-    public static final By signInBtn = By.xpath("//input[@name='commit']");
 
     public static final By signUp = By.xpath("/html/body/div[1]/div[3]/header/div/div[2]/div/div/a");
 
